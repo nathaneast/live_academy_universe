d978cc3db5a99573c8857b8b6821ca3548ce3cb3
nathaneast <ahrl_ahrl@naver.com>
2024-09-27 15:46:34 +0900

    feat: Video Input form mange state, ui

--- src/app/workbook/_formComponents/Video.tsx
+++ src/app/workbook/_formComponents/Video.tsx
@@ -1,10 +1,18 @@
 "use client";
 
 import { useFormContext } from "react-hook-form";
-import { Button } from "components/ui/button";
+import { Button } from "@/components/ui/button";
+import {
+  FormField,
+  FormItem,
+  FormLabel,
+  FormControl,
+  FormMessage,
+} from "@/components/ui/form"; // 필요한 모듈 추가
+import { Input } from "@/components/ui/input";
 
 const Video = () => {
-  const { register, setValue, getValues } = useFormContext();
+  const { control, register, setValue, getValues } = useFormContext();
 
   const handleAddWorkBookCard = () => {
     const currentWorkBookCards = getValues("workBookCards") || [];
@@ -19,13 +27,30 @@ const Video = () => {
   return (
     <section>
       <div>
-        <span>유튜브 url 입력</span>
-        <input
-          type="text"
-          {...register("videoUrl", { required: "URL은 필수입니다." })}
-        />
-        <Button>강의 등록</Button>
         <Button onClick={handleAddWorkBookCard}>mock_card_add</Button>
+      </div>
+
+      <div>
+        <FormField
+          control={control}
+          name="videoUrl"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>유튜브 URL</FormLabel>
+              <FormControl>
+                <div className="flex">
+                  <Input
+                    placeholder="유튜브 url을 입력 해주세요"
+                    {...field}
+                    {...register("videoUrl", { required: "URL은 필수입니다." })} // 유효성 검사 추가
+                  />
+                  <Button>강의 등록</Button>
+                </div>
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
       </div>
 
       <div>비디오 플레이어</div>
