b3ec4dabbab54665d066093ea886aae74003676f
nathaneast <ahrl_ahrl@naver.com>
2024-11-04 17:54:16 +0900

    fix: change Card UI at WorkBookCard

--- src/app/workbook/_formComponents/(WorkBookCardList)/WorkBookCard.tsx
+++ src/app/workbook/_formComponents/(WorkBookCardList)/WorkBookCard.tsx
@@ -3,6 +3,7 @@
 import Image from "next/image";
 import { Textarea } from "@/components/ui/textarea";
 import useTextArea from "@/components/hooks/useTextArea";
+import { Card, CardContent } from "@/components/ui/card";
 
 interface Props {
   id: string;
@@ -15,30 +16,32 @@ const WorkBookCard = ({ id, imagePath, memoRegister, deleteCard }: Props) => {
   const { textareaRef, handleInputAutoHeight } = useTextArea();
 
   return (
-    <div key={id} className="flex flex-col gap-2 border border-black">
-      <div className="relative">
-        <span
-          onClick={deleteCard}
-          className="absolute top-4 right-4 cursor-pointer text-xl"
-        >
-          ❌
-        </span>
-      </div>
-      <Image
-        src={imagePath}
-        alt="워크북 카드 이미지"
-        layout="responsive"
-        width={100}
-        height={100}
-      />
-      <Textarea
-        {...memoRegister()}
-        ref={textareaRef}
-        className="resize-y"
-        rows={3}
-        onInput={handleInputAutoHeight}
-      />
-    </div>
+    <Card key={id}>
+      <CardContent className="flex flex-col gap-2 p-1">
+        <div className="relative">
+          <span
+            onClick={deleteCard}
+            className="absolute top-4 right-4 cursor-pointer text-xl"
+          >
+            ❌
+          </span>
+        </div>
+        <Image
+          src={imagePath}
+          alt="워크북 카드 이미지"
+          layout="responsive"
+          width={100}
+          height={100}
+        />
+        <Textarea
+          {...memoRegister()}
+          ref={textareaRef}
+          className="resize-y"
+          rows={3}
+          onInput={handleInputAutoHeight}
+        />
+      </CardContent>
+    </Card>
   );
 };
 
